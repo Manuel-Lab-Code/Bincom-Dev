80aa2f1ee01900461f3f75f5d258199a0c6066c7
Manuel Ⓜ️™ <104109789+Manuel-Lab-Code@users.noreply.github.com>
2022-08-05 17:04:12 -0400

    first commit

---
+++ interviw project/script.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector("#menu");
+const navMenu = document.querySelector(".nav-menu");
+
+menu.addEventListener("click", () => {
+	menu.classList.toggle(".active");
+	navMenu.classList.toggle(".active");
+
+})
+
+
+
